a3c8938c169dd58b3742e343499d467b2246b10c
akshadpai <akshadpai01@gmail.com>
2025-06-16 16:15:40 -0500

    Use $denseRank to avoid gaps in ranks

--- driven/storage/adapter_scores.go
+++ driven/storage/adapter_scores.go
@@ -31,7 +31,7 @@ func (a *Adapter) GetScore(orgID string, appID string, userID string) (*model.Sc
 		bson.D{{Key: "$setWindowFields", Value: bson.M{
 			"sortBy": bson.M{"score": -1},
 			"output": bson.M{
-				"rank": bson.M{"$rank": bson.M{}},
+				"rank": bson.M{"$denseRank": bson.M{}},
 			},
 		}}},
 		bson.D{{Key: "$match", Value: bson.M{"user_id": userID}}},
@@ -66,7 +66,7 @@ func (a *Adapter) GetScores(orgID string, appID string, limit *int, offset *int)
 		bson.D{{Key: "$setWindowFields", Value: bson.M{
 			"sortBy": bson.M{"score": -1},
 			"output": bson.M{
-				"rank": bson.M{"$rank": bson.M{}},
+				"rank": bson.M{"$denseRank": bson.M{}},
 			},
 		}}},
 
@@ -97,7 +97,7 @@ func (a *Adapter) GetTopAndLocalScores(orgID string, appID string, userID string
 		bson.D{{Key: "$setWindowFields", Value: bson.M{
 			"sortBy": bson.M{"score": -1},
 			"output": bson.M{
-				"rank": bson.M{"$rank": bson.M{}},
+				"rank": bson.M{"$denseRank": bson.M{}},
 				"pivotScore": bson.M{"$max": bson.M{
 					"$cond": bson.A{
 						bson.M{"$eq": bson.A{"$user_id", userID}},

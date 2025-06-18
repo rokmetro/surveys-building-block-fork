fad6fbdf622a661c2d2cfb5fc3e30bcb08e9f21f
Ryan Oberlander <reoberlander2@gmail.com>
2025-06-18 16:54:04 -0500

    update comment explaining no index on leaderboards collection [#29]

--- driven/storage/database.go
+++ driven/storage/database.go
@@ -209,7 +209,7 @@ func (d *database) applyScoresChecks(scores *collectionWrapper) error {
 func (d *database) applyLeaderboardsChecks(leaderboards *collectionWrapper) error {
 	d.logger.Info("apply leaderboards checks.....")
 
-	// do not need an index on leaderboards collection for now - index on _id is sufficient for current operations
+	// do not need an index on leaderboards collection for now - index on _id is sufficient for current operations (UpdateLeaderboard and DeleteLeaderboard)
 
 	d.logger.Info("leaderboards passed")
 	return nil

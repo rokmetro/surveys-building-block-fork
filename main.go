778cbb15b74b79322f43b2b298e729b6274a6a6b
Ben Dworkin <ben.s.dworkin@gmail.com>
2025-06-20 11:00:35 -0500

    update changelog and secrets baseline

--- main.go
+++ main.go
@@ -112,12 +112,10 @@ func main() {
 			logger.Fatalf("Error initializing remote service account loader: %v", err)
 		}
 
-		print(serviceAccountLoader)
-
-		// serviceAccountManager, err = authservice.NewServiceAccountManager(&authService, serviceAccountLoader)
-		// if err != nil {
-		// 	logger.Fatalf("Error initializing service account manager: %v", err)
-		// }
+		serviceAccountManager, err = auth.NewServiceAccountManager(&authService, serviceAccountLoader)
+		if err != nil {
+			logger.Fatalf("Error initializing service account manager: %v", err)
+		}
 	}
 
 	// Notifications adapter

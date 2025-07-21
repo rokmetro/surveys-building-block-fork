98ea7855c9fb3e530c9e6e2d71e8a293e424f7eb
Ryan Oberlander <reoberlander2@gmail.com>
2025-07-21 12:14:26 -0500

    fix notification body text [#45]

--- core/app_client.go
+++ core/app_client.go
@@ -262,7 +262,11 @@ func (a appClient) sendFashionQuizNotifications(orgID string, appID string, user
 					if score != nil {
 						points = int(score.Score - oldScore.Score)
 					}
-					body := fmt.Sprintf("@%s just scored %d points in today's Runway Genius. Can you outplay them?", username, points)
+					pointsString := "points"
+					if points == 1 {
+						pointsString = "point"
+					}
+					body := fmt.Sprintf("@%s just scored %d %s in today's Runway Genius. Can you outplay them?", username, points, pointsString)
 
 					message := model.NotificationMessage{
 						OrgID: orgID,
